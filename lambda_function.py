f7a28433567bda08305801c7349df3440e7b462d
Matt Auerbach <tennisfan0526@gmail.com>
2015-10-12 13:15:31 -0700

    bug fixes

--- lambda_function.py
+++ lambda_function.py
@@ -1,13 +1,11 @@
 
 import boto3
-import os
 import urllib2
 import random
 
 from boto3.session import Session
-from twilio import twiml
 from twilio.rest import TwilioRestClient
-from boto3.dynamodb.conditions import Key, Attr
+from boto3.dynamodb.conditions import Key
 
 # create Twilio session
 # 1) Add Twilio Keys
@@ -25,14 +23,10 @@ table_users = dynamodb.Table('table-name')
 
 def lambda_handler(event, context):
 
-    body = event['body']
+    message = event['body']
     fromNumber = event['fromNumber']
     image = event['image']
     numMedia = event['numMedia']
-
-    # create response
-    response = twiml.Response()
-    response.ContentType = "application/xml"
 
     # check if we have their number
     response_dynamo = table_users.query(KeyConditionExpression=Key('fromNumber').eq(fromNumber))
@@ -40,13 +34,11 @@ def lambda_handler(event, context):
     # a new user
     if response_dynamo['Count'] == 0:
         if len(message) == 0:
-            response.message("Please send us an SMS with your name first!")
-            return str(response)
+            return "Please send us an SMS with your name first!"
         else:
             name = message.split(" ")
             table_users.put_item(Item={'fromNumber': fromNumber, 'name': name[0]})
-            response.message("We've added {0} to the system! Now send us a selfie! ".format(name[0]))
-            return str(response)
+            return "We've added {0} to the system! Now send us a selfie! ".format(name[0])
     else:
         name = response_dynamo['Items'][0]['name']
 
@@ -57,14 +49,13 @@ def lambda_handler(event, context):
         image_ = urllib2.urlopen(twilio_pic)
         # 3) Add S3 Bucket
         bucket = "s3-bucket"
-        key = 'ingest-images/' + str(fromNumber.replace('+', '')) + '/' + str(random.getrandbits(50)) + '.png'
-        resp_url = 'https://s3-us-west-2.amazonaws.com/{0}/{1}'.format(bucket, str(key))
-        twilio_resp = 'Hi {0}, your S3 link: '.format(name) + 'https://s3-us-west-2.amazonaws.com/{0}/{1}'.format(bucket, str(key))
+        key = "ingest-images/" + str(fromNumber.replace('+', '')) + "/" + str(random.getrandbits(50)) + ".png"
+        resp_url = "https://s3-us-west-2.amazonaws.com/{0}/{1}".format(bucket, str(key))
+        twilio_resp = "Hi {0}, your S3 link: ".format(name) + "https://s3-us-west-2.amazonaws.com/{0}/{1}".format(bucket, str(key))
         # build meta data
         m_data = {'fromNumber': fromNumber, 'url': resp_url, 'name': name}
         s3.Bucket(bucket).put_object(Key=key, Body=image_.read(), ACL='public-read', ContentType='image/png', Metadata=m_data)
     else:
-        response.message("No image found. :( ")
         twilio_resp = 'No image found'
     
     return twilio_resp
